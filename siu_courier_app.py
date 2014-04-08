478fbdcb7194a1d19e46b63cdeae9b12bc3cbb38
Jonas Andersson <Jonas123@gmail.com>
2014-04-08 17:49:32 -0500

    launches user interface

---
+++ siu_courier_app.py
@@ -0,0 +1,24 @@
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+from PySide.QtDeclarative import QDeclarativeView
+import nurse
+import patient
+
+
+class Courier(QDeclarativeView):
+	def __init__(self):
+		super(Courier, self).__init__()
+		self.setWindowTitle("SIU Courier Telemedical Solution")
+
+def main():
+	app = QApplication(sys.argv)
+	nurse1 = nurse.Nurse()
+	view = Courier()
+	view.rootContext().setContextProperty('nurse', nurse1) #this needs to happen before setsource or errors get thrown
+	view.setSource('main.qml')
+	view.show()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	main()
