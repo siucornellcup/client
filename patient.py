11f953b780e182d2c1535fd03c45b3aafbd25ba1
Jonas Andersson <Jonas123@gmail.com>
2014-04-25 17:38:28 -0500

    added first and last name

--- patient.py
+++ patient.py
@@ -6,10 +6,14 @@ from dbTools import create_patient
 class Patient(QObject):
 	def __init__(self):
 		self.name = None
+		self.first_name = None
+		self.last_name = None
 		self.village = None
 		self.dob = None
 		self.fingerprint = None
 		self.gender = None
+		self.photo_id_path = None
+		self.photo_id = None
 		QObject.__init__(self)
 
 	@Slot()
@@ -22,12 +26,26 @@ class Patient(QObject):
 	def create_record(self):
 		create_patient(self.name, self.village, self.gender, self.fingerprint)
 
+	@Slot()
+	def load_photo_id(self, photo_path):
+		self.photo_id = open(photo_path,"rb").read()
+
 
 	def set_name(self, name):
 		self.name = name
 		print self.name #DEBUGGING
 	def get_name(self):
 		return self.name
+
+	def set_first_name(self, first_name):
+		self.first_name = first_name
+	def get_first_name(self):
+		return self.first_name
+
+	def set_last_name(self, last_name):
+		self.last_name = last_name
+	def get_last_name(self):
+		return self.last_name
 
 	def set_village(self, village):
 		self.village = village
@@ -46,6 +64,11 @@ class Patient(QObject):
 	def get_gender(self):
 		return self.gender
 
+	def set_photo_id_path(self, photo_id_path):
+		self.photo_id_path = photo_id_path
+	def get_photo_id_path(self):
+		return self.photo_id_path
+
 	@Signal
 	def name_changed(self):
 		pass
@@ -58,8 +81,20 @@ class Patient(QObject):
 	@Signal
 	def gender_changed(self):
 		pass
+	@Signal
+	def photo_id_path_changed(self):
+		pass
+	@Signal
+	def first_name_changed(self):
+		pass
+	@Signal
+	def last_name_changed(self):
+		pass
 
 	p_name = Property(unicode, get_name, set_name, notify=name_changed)
+	p_first_name = Property(unicode, get_first_name, set_first_name, notify=first_name_changed)
+	p_last_name = Property(unicode, get_last_name, set_last_name, notify=last_name_changed)
 	p_village = Property(unicode, get_village, set_village, notify=village_changed)
 	p_dob = Property(unicode, get_dob, set_dob, notify=dob_changed)
 	p_gender = Property(unicode, get_gender, set_gender, notify=gender_changed)
+	p_photo_id_path = Property(unicode, get_photo_id_path, set_photo_id_path, notify=photo_id_path_changed)
