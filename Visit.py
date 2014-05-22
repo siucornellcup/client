f80e4d6fe9d295682f2212d7f5e8628d1fea3200
Jonas Andersson <Jonas123@gmail.com>
2014-05-21 23:05:24 -0500

    Remove SpO2 sensor code, Create pulse_rate setter

--- Visit.py
+++ Visit.py
@@ -19,44 +19,55 @@ class Visit(QObject):
 		QObject.__init__(self)
 
 	## Getters & Setters
+	# Old code for SpO2 sensor
+	# @Slot()
+	# def read_pulse_sensor(self):
+	# 	print 'pulse scan initiated'
+	# 	ser = serial.Serial('/dev/ttyACM0', 9600)
+	# 	ser.flushOutput()
+	# 	datum = ser.readline()
+	# 	self.pulse_rate = datum
+	# 	print datum
 
-	@Slot()
-	def read_pulse_sensor(self):
-		print 'pulse scan initiated'
-		ser = serial.Serial('/dev/ttyACM0', 9600)
-		ser.flushOutput()
-		datum = ser.readline()
-		self.pulse_rate = datum
-		print datum
+	def set_pulse(self, pulse):
+		self.pulse_rate = pulse
+		print "pulse: " + self.pulse_rate #debugging
 
 	def get_pulse(self):
 		return int(self.pulse_rate)
 
 	def set_height(self, height):
 		self.height = height
-		print self.height
+		print "height: " + self.height #debugging
+
 	def get_height(self):
 		return self.height
 
 	def set_weight(self, weight):
 		self.weight = weight
+		print "weight: " + self.weight #debugging
+
 	def get_weight(self):
 		return self.weight
 
 	def set_visit_id(self, visit_id):
 		self.visit_id = visit_id
+
 	def get_visit_id(self):
 		return self.visit_id
 
 	@Signal
 	def weight_changed(self):
 		pass
+
 	@Signal
 	def height_changed(self):
 		pass
+
 	@Signal
 	def visit_id_changed(self):
 		pass
+
 	@Signal
 	def pulse_rate_changed(self):
 		pass
@@ -64,4 +75,4 @@ class Visit(QObject):
 	v_id = Property(unicode, get_visit_id, set_visit_id, notify=visit_id_changed)
 	v_weight = Property(unicode, get_weight, set_weight, notify=weight_changed)
 	v_height = Property(unicode, get_height, set_height, notify=height_changed)
-	v_pulse_rate = Property(int, get_pulse, read_pulse_sensor, notify=pulse_rate_changed)
+	v_pulse_rate = Property(int, get_pulse, set_pulse, notify=pulse_rate_changed)
